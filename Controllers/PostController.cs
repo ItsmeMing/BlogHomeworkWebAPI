f7d971c4720ae246f2b2a2b6107967e74a572f88
ItsmeMing <ngocminh.vu.520@gmail.com>
2024-03-13 04:18:07 +0700

    Add project files.

---
+++ Controllers/PostController.cs
@@ -0,0 +1,35 @@
+﻿using DataAccess.Blog.Entities;
+using DataAccess.Blog.IServices;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogHomeworkWebAPI.Controllers
+{
+	[Route("posts")]
+	[ApiController]
+	public class PostController : ControllerBase
+	{
+		private IPostServices _postServices;
+		public PostController(IPostServices postServices) // TIÊM 
+		{
+			_postServices = postServices;
+		}
+
+			
+		[HttpGet()]
+		public async Task<ActionResult> Post_Getlist([FromQuery] string? post_id)
+		{
+
+			var list = new List<Post>();
+			if (post_id == null)
+			{
+				list = await _postServices.GetPosts(null);
+			}
+			else
+			{
+				list = await _postServices.GetPosts(Int32.Parse(post_id)); 
+			}
+			return Ok(list);
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+using DataAccess.Blog.EntityFramework;
+using DataAccess.Blog.IServices;
+using DataAccess.Blog.Services;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<BlogDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("ConnStr")));
+builder.Services.AddTransient<IPostServices, PostServices>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+	app.UseSwagger();
+	app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
